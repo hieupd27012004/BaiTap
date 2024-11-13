758f238c0c3533d67e06ecee64f65e7b185ad7bb
Phạm Hiếu <phamduchieu27012004@gmail.com>
2024-11-13 19:34:11 +0700

    Cách mở file

--- 3.GUI/Program.cs
+++ 3.GUI/Program.cs
@@ -13,7 +13,8 @@ namespace _3.GUI
         {
             ApplicationConfiguration.Initialize();
             Application.Run(new FRDangNhap());
-            //Hieuphph40428@gmail.com
+            //Hieuphph40428@gmail.com mk 123
+            // M?i ngý?i dùng Clone using the web URL ð? m? file nhé n?u t?i xu?ng theo t?p zip th? ph?i b? ch?n ? Windows Explorer ch?n .resx r?i chu?t trái ?n b? ch?n 
         }
     }
 }
